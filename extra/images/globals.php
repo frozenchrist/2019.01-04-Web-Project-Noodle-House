b099e8f92c0ae6cdf1a86ab1449a17afb29c53cf
frozenchrist <48806302+frozenchrist@users.noreply.github.com>
2019-09-03 16:17:52 -0400

    Add files via upload

---
+++ extra/images/globals.php
@@ -0,0 +1,31 @@
+<?php 
+
+//contains arrays that store the filename of dishes.
+
+
+$noodles = array(
+  'beef-noodles.png',
+  'veggie-noodles.png',
+  'fish-noodles.png',
+  'chicken-noodles.png',
+  'shrimp-noodles.png',
+  'spicyseafood-noodles.png',
+  'meatball-noodles.png',
+  'pork-noodles.png',
+  'mushroom-noodles.png',
+);
+
+$rice = array(
+  'beef-rice.png',
+  'veggie-rice.png',
+  'fish-rice.png',
+  'chicken-rice.png',
+  'shrimp-rice.png',
+  'spicyseafood-rice.png',
+  'meatball-rice.png',
+  'pork-rice.png',
+  'mushroom-rice.png',
+  
+);
+
+?>
